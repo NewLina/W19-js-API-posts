dbaeb97909c88c727415bd02219fe6e792c1c6ca
PolinaПолина <prybak4972gmail.com>
2023-03-15 17:00:08 +0300

    assignment1

---
+++ assignment1/script/index.js
@@ -0,0 +1,25 @@
+const main = document.querySelector('.wrapper');
+
+function makeLayout(object) {
+    let displayPost = '';
+        displayPost = `
+            <div class='post'>
+                <div class='post__heading'>Заголовок: ${object.title}</div>
+                <div class='post__content'><span>Статья:</span> ${object.body}</div>
+            </div>
+        `
+        main.innerHTML += displayPost;
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .then(post => {
+            post.forEach(item => {
+                makeLayout(item);
+            });
+        })
+        .catch((error) => alert(error))
+})
